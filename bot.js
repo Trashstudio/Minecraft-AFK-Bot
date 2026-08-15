5e8781269664d0ace70825d2fe630911827ec075
Trashstudio <soviet2306@gmail.com>
2026-08-15 21:01:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,8 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.write("Bot is running!");
+  res.end();
+}).listen(process.env.PORT || 3000);
 const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
